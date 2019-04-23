540cdd10585f0fa1b4770687d4a1d1e8d1c87421
Jan Wychowaniak <43786923+janwychowaniak@users.noreply.github.com>
2019-04-23 22:42:30 +0200

    translation, wherever needed

--- ffklip/jwffklip.sh
+++ ffklip/jwffklip.sh
@@ -3,14 +3,13 @@
 ## media: ffmpeg
 
 
-jak_uzywac() {
+usage() {
 cat 1>&2 <<EOF
 
-./`basename $0` [NAZWA_KLIPU] [TEMPO] <OUTPUT_DIR>
+./`basename $0` [CLIP_NAME] [TEMPO] <OUTPUT_DIR>
 
-    Skrypt kompresuje KLIP
-    popedzajac go jednoczesnie na zadane TEMPO
-    pakujac rezultat do ./, ew. do OUTPUT_DIR
+    The script encodes the CLIP file, changing its tempo by the TEMPO factor
+    and putting the results to the current directory (./, or optionally to OUTPUT_DIR)
 
     Use JWFFKLIPARGSV and JWFFKLIPARGSA env vars for
     passing additional processing parameters:
@@ -22,12 +21,12 @@ EOF
 
 
 if [ $# -ne 2 -a $# -ne 3 ]; then
-	jak_uzywac
+	usage
 	exit 1
 fi
 
 if [ $# -gt 0 ] && ([ "$1" == "-h" ] || [ "$1" == "-help" ] || [ "$1" == "--help" ]); then
-	jak_uzywac; exit 1
+	usage; exit 1
 fi
 
 
@@ -42,7 +41,7 @@ if [[ ! -d "$SCRIPTDIR" ]]; then SCRIPTDIR="$PWD"; fi
 # ----------------------------------------------------------------------------------------
 
 
-NAZWA_KLIPU=$1
+CLIP_NAME=$1
 TEMPO=$(echo "$2" | tr "," ".")
 
 if [ $# -eq 3 ]; then
@@ -59,21 +58,21 @@ fi
 # atempo=$(get_atempo $TEMPO)
 # vtempo=$(get_vtempo $TEMPO)
 
-nazwa_in=$(echo $NAZWA_KLIPU)
-nazwa_inter=$(echo $RANDOM.avi)
-nazwa_out=$(get_nazwa_out $NAZWA_KLIPU $OUTPUT_DIR)
+name_in=$(echo $CLIP_NAME)
+name_inter=$(echo $RANDOM.avi)
+name_out=$(get_nazwa_out $CLIP_NAME $OUTPUT_DIR)
 
-#~ echo "atempo      = $atempo"
-#~ echo "vtempo      = $vtempo"
-#~ echo "nazwa_in    = $nazwa_in"
-#~ echo "nazwa_inter = $nazwa_inter"
-#~ echo "nazwa_out   = $nazwa_out"
+#~ echo "atempo     = $atempo"
+#~ echo "vtempo     = $vtempo"
+#~ echo "name_in    = $name_in"
+#~ echo "name_inter = $name_inter"
+#~ echo "name_out   = $name_out"
 
 FILTER_COMPLEX=$(get_filter_complex $TEMPO)
 
 # step 1
-echo "ffmpeg -i $nazwa_in $FILTER_COMPLEX -q:v 0 $JWFFKLIPARGSV -q:a 0 $JWFFKLIPARGSA $nazwa_inter ;"
+echo "ffmpeg -i $name_in $FILTER_COMPLEX -q:v 0 $JWFFKLIPARGSV -q:a 0 $JWFFKLIPARGSA $name_inter ;"
 # step 2
-echo "ffmpeg -i $nazwa_inter -c:v libx264 -preset veryfast -crf 26 -c:a aac -b:a 64k -ac 1 -sn -strict experimental $nazwa_out ;"
+echo "ffmpeg -i $name_inter -c:v libx264 -preset veryfast -crf 26 -c:a aac -b:a 64k -ac 1 -sn -strict experimental $name_out ;"
 # cleanup
-echo "rm $nazwa_inter ;"
+echo "rm $name_inter ;"
